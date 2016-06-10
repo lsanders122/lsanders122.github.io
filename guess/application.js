a8c1208e698c132ed1bea2fc4c2f7e09653d8e2b
lsanders122 <lsanders@kippnyccp.org>
2016-06-10 11:01:29 -0400

    Update application.js

--- guess/application.js
+++ guess/application.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
-	if random_num= 
+	$("#submit").click(function(){
+		var user_guess = $("input[name=guess]").val();
+		if (user_guess == random_num){
+			alert("You got it!");
+		} else if() {
+			
+		} else {
+			
+		}
+		$("input[name=guess]").val("").focus();
+	})
 
 
 
